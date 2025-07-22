9564f5e753f024a14d9d8e30757f45d71d55671d
Mondchaot <mondchaot@gmx.de>
2025-07-22 23:14:43 +0200

    Add files via upload

---
+++ ModuleCheckWidget.kt
@@ -0,0 +1,10 @@
+
+package com.pertinax.widgets
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ModuleCheckWidget() {
+    Text("3D Light UI – ModuleCheckWidget Widget aktiv")
+}
